aaacfedcd26d82bd97340a9fc2c6301b8847e6c3
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-01-22 17:20:29 +0530

    Added Destination model for travel locations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='destinations/')
+    price_range = models.CharField(max_length=50)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+
+    def __str__(self):
+        return f"{self.name}, {self.country}"
+
