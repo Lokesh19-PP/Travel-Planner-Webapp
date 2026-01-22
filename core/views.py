ac325ce0317cc9611b7224814692927b8e25d96a
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-01-22 20:12:29 +0530

    add destination cards section with search bar and styling

--- core/views.py
+++ core/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .models import Destination
 
 def home(request):
-    destinations = Destination.objects.all()
-    context = {
-        'destinations': destinations
-    }
-    return render(request, 'core/home.html', context)
+    query = request.GET.get('search', '')  # Get search term from URL
+    if query:
+        destinations = Destination.objects.filter(name__icontains=query)
+    else:
+        destinations = Destination.objects.all()
+    return render(request, 'core/home.html', {'destinations': destinations, 'query': query})
+
 
