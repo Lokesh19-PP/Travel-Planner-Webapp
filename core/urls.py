836ca398c69a328400fdef78291ce36d5246931a
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-01-27 21:13:11 +0530

    Fix profile page and rollback previous errors

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path("signup/", views.signup_view, name="signup"),
     path('profile/', views.profile_view, name='profile'),
+    #path('destinations/', views.destinations, name='destinations')
 ]
