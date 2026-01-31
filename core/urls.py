d03cd5d5dee7fa64e4afff1aec89574eb9ad82f9
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-01-31 15:47:16 +0530

    add Url for Itinerary

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('destinations/', views.destination_list, name='destination_list'),
     path('destinations/<slug:slug>/', views.destination_detail, name='destination_detail'),
+    path('itineraries/', views.itinerary_list, name='itinerary_list'),
+    path('itinerary/<int:pk>/', views.itinerary_detail, name='itinerary_detail'),
+    path('itinerary/add/', views.itinerary_create, name='itinerary_create'),
 ]
