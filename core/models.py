b5fbc79cd2fede49ad148efd04ea7b863d282d27
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-01-31 15:44:28 +0530

    create model for Itinerary

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 class Destination(models.Model):
     name = models.CharField(max_length=100)
@@ -18,3 +19,14 @@ class Destination(models.Model):
 
     def __str__(self):
         return f"{self.name}, {self.country}"
+
+class Itinerary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='itineraries')
+    destinations = models.ManyToManyField('Destination', related_name='itineraries')
+    name = models.CharField(max_length=100)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} by {self.user.username}"
