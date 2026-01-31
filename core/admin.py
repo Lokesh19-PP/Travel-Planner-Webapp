f553340375bf353ff2337e01048d9e2471f17749
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-01-31 15:45:59 +0530

    Create admin model for Itinerary

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Destination
+from .models import Itinerary
 
 @admin.register(Destination)
 class DestinationAdmin(admin.ModelAdmin):
@@ -7,3 +8,8 @@ class DestinationAdmin(admin.ModelAdmin):
     search_fields = ('name', 'country')
     list_filter = ('country', 'price_range')
 
+@admin.register(Itinerary)
+class ItineraryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'start_date', 'end_date', 'created_at')
+    list_filter = ('start_date', 'end_date', 'user')
+    search_fields = ('name', 'user__username')
