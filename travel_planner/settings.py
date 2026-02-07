af9baa3ea20e5605fb5190ede6b4469bea40aae6
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-02-07 21:23:23 +0530

    Fix bug of Redirection of Login page

--- travel_planner/settings.py
+++ travel_planner/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_URL = '/login/'    # or reverse_lazy('login')
+LOGIN_REDIRECT_URL = '/'  # after login, redirect here
+LOGOUT_REDIRECT_URL = '/'  # after logout
