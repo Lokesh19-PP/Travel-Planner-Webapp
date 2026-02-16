9769d6b285bbb298308bed735670639a08ce0053
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-02-16 23:44:20 +0530

    feat: update Favorite model for destinations

--- core/models.py
+++ core/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User
-from .models import Destination
 
 class Destination(models.Model):
     name = models.CharField(max_length=100)
@@ -48,7 +47,7 @@ class Review(models.Model):
     
 class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
-    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+    destination = models.ForeignKey('Destination', on_delete=models.CASCADE)
     added_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
